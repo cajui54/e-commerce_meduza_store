d2c8b433137111f16da5aecefb81afd6481cb0ae
cajui54 <cajui54@gmail.com>
2025-06-15 16:51:42 -0300

    feat: add PromoBanner and Section component

---
+++ app/(home)/_components/promo-banner.tsx
@@ -0,0 +1,17 @@
+import Image, { ImageProps } from 'next/image';
+import React from 'react';
+
+const PromoBanner = ({ alt, ...props }: ImageProps) => {
+  return (
+    <Image
+      alt={alt}
+      width={0}
+      height={0}
+      className="h-auto w-full px-5"
+      sizes="100vw"
+      {...props}
+    />
+  );
+};
+
+export default PromoBanner;
