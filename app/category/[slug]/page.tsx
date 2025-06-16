796ff6c71ed4443d9b0975b6b30ba5c84cb0bc52
cajui54 <cajui54@gmail.com>
2025-06-16 15:29:41 -0300

    feat: add page to show category products

---
+++ app/category/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import { CATEGORY_ICON } from '@/app/constants/category-icon';
+import { computeProductTotalPrice } from '@/app/helpers/product';
+import { Badge } from '@/components/ui/badge';
+import ProductItem from '@/components/ui/product-item';
+import { prismaClient } from '@/lib/prisma';
+import { ShapesIcon } from 'lucide-react';
+import { notFound } from 'next/navigation';
+import React from 'react';
+interface CategoryPageProps {
+  params: {
+    slug: string;
+  };
+}
+const CategoryPage = async ({ params }: CategoryPageProps) => {
+  const category = await prismaClient.category.findFirst({
+    where: { slug: params.slug },
+    include: {
+      products: true,
+    },
+  });
+  if (!category) return notFound();
+  return (
+    <div className="space-y-8 p-5">
+      <Badge
+        variant={'outline'}
+        className="gap-1 border-2 border-gray-400 px-3 py-[0.375rem] text-base uppercase"
+      >
+        {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+        {category?.name}
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {category?.products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
