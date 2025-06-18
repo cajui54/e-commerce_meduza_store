f05dc845f977aedc1e5a332214801495a485bb8d
cajui54 <cajui54@gmail.com>
2025-06-18 12:23:46 -0300

    feat: ensure user goes to product details page on product click

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import { prismaClient } from '@/lib/prisma';
+import React from 'react';
+
+interface ProductDetailsPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const ProductDetailsPage = async ({ params }: ProductDetailsPageProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: { slug: params.slug },
+  });
+  if (!product) return null;
+  return <div>{product?.name}</div>;
+};
+
+export default ProductDetailsPage;
--- components/ui/product-item.tsx
+++ components/ui/product-item.tsx
@@ -3,51 +3,54 @@ import Image from 'next/image';
 import React from 'react';
 import { Badge } from './badge';
 import { ArrowDownIcon } from 'lucide-react';
+import Link from 'next/link';
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
 }
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
-        <Image
-          src={product.imageUrls[0]}
-          alt={product.name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[90%]"
-          style={{ objectFit: 'contain' }}
-        />
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-            <ArrowDownIcon size={14} />
-            {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <p className="truncate">{product.name}</p>
-        <div className="flex items-center gap-2 truncate">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className="font-semibold">
-                R$: {product.totalPrice.toFixed(2)}
-              </p>
-              <p className="truncate text-xs line-through opacity-75">
-                R$: {Number(product.basePrice).toFixed(2)}
-              </p>
-            </>
-          ) : (
-            <p className="text-sm font-semibold">
-              R$: {product.basePrice.toFixed(2)}
-            </p>
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex flex-col gap-4">
+        <div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            alt={product.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[90%]"
+            style={{ objectFit: 'contain' }}
+          />
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+              <ArrowDownIcon size={14} />
+              {product.discountPercentage}%
+            </Badge>
           )}
         </div>
+
+        <div className="flex flex-col gap-1">
+          <p className="truncate">{product.name}</p>
+          <div className="flex items-center gap-2 truncate">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="font-semibold">
+                  R$: {product.totalPrice.toFixed(2)}
+                </p>
+                <p className="truncate text-xs line-through opacity-75">
+                  R$: {Number(product.basePrice).toFixed(2)}
+                </p>
+              </>
+            ) : (
+              <p className="text-sm font-semibold">
+                R$: {product.basePrice.toFixed(2)}
+              </p>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
